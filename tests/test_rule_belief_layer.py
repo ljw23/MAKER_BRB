0ecf0f7c894320a2c083252cb7deeac0165c7381
ljw23 <ljw23@mail.ustc.edu.cn>
2021-06-26 01:59:44 +0800

    add brb layer

--- tests/test_rule_belief_layer.py
+++ tests/test_rule_belief_layer.py
@@ -4,12 +4,14 @@ import torch
 def test_weight_layer():
     num_h = 3
     num_k = 20
-    W_rule_act = torch.rand(num_k)
+    batch_size =8
+    W_rule_act = torch.rand(batch_size,num_k)
 
     rule_belief_layer = Rule_belief_layer(num_k, num_h)
     output = rule_belief_layer(W_rule_act)
 
     print(output)
+    print(output.shape)
 
 if __name__ == '__main__':
     test_weight_layer()
